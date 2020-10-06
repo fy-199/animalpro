6b35778fef8d89441cb70f1955f5025dbd30e445
fy199 <fatihyuzbasioglu2@gmail.com>
2020-10-06 21:05:57 +0300

    ikinci deneme

---
+++ script.js
@@ -0,0 +1,9 @@
+let myAge = 23;
+let earlyYears = 2;
+earlyYears *= 10.5;
+laterYears = myAge - 2;
+laterYears *= 4;
+let myAgeInDogYears = (earlyYears + laterYears);
+let myName = "Memati";
+let lol = myName.toLowerCase();
+console.log(`My name is ${lol}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`)
